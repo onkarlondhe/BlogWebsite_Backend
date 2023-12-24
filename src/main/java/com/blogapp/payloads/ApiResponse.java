970e05cb09c807505ac9afa9b535a95fd4379605
onkarlondhe <onkarrameshlondhe@gmail.com>
2023-12-24 15:48:59 +0530

    Revert "Revert "#5.create custom exception and hadle request with userId.""
    This reverts commit dae16452f4cf0085d0a410cc979a60afb56916b1.

---
+++ src/main/java/com/blogapp/payloads/ApiResponse.java
@@ -0,0 +1,14 @@
+package com.blogapp.payloads;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApiResponse {
+
+	private String message;
+	private boolean success;
+}
